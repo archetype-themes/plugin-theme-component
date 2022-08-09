aa39ad147b6b66eb3478597c3e399f4054dabfe1
Frédéric Léveillée <frederic@archetypethemes.co>
2022-08-09 17:20:43 -0400

    Added renders to Section

--- models/Section.js
+++ models/Section.js
@@ -3,23 +3,57 @@ import Snippet from './Snippet.js'
 class Section extends Snippet {
   constructor () {
     super()
+    this.liquidCode = ''
+    this.renders = []
     this.snippetFiles = []
   }
 
+  /**
+   * Get Schema File
+   * @returns {string}
+   */
   get schemaFile () {
     return this._schemaFile
   }
 
+  /**
+   * Set Schema File
+   * @param {string} schemaFile
+   */
   set schemaFile (schemaFile) {
     this._schemaFile = schemaFile
   }
 
+  /**
+   * Get SnippetFiles
+   * @returns {string[][]}
+   */
   get snippetFiles () {
     return this._snippetFiles
   }
 
+  /**
+   * Set Snippet Files
+   * @param {string[][]} snippetFiles
+   */
   set snippetFiles (snippetFiles) {
     this._snippetFiles = snippetFiles
+  }
+
+  /**
+   * Get Renders
+   * @returns {Render[]}
+   */
+  get renders () {
+    return this._renders
+  }
+
+  /**
+   * Set Renders
+   * @param {Render[]} renders
+   */
+  set renders (renders) {
+    this._renders = renders
   }
 
 }
