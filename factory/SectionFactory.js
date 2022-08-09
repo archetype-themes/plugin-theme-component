30c68da7c1b36f30db127caaf4f7361a66138479
Frédéric Léveillée <frederic@archetypethemes.co>
2022-08-09 17:35:04 -0400

    SectionFactory.js now handles liquidCode and Renders analysis

--- factory/SectionFactory.js
+++ factory/SectionFactory.js
@@ -1,7 +1,12 @@
+import { readFile } from 'node:fs/promises'
+
 import Section from '../models/Section.js'
-import { detectSectionFolder } from '../utils/SectionUtils.js'
-import FileUtils from '../utils/FileUtils.js'
+
 import ComponentUtils from '../utils/ComponentUtils.js'
+import FileUtils, { FILE_ENCODING_OPTION } from '../utils/FileUtils.js'
+import LiquidUtils from '../utils/LiquidUtils.js'
+import logger from '../utils/Logger.js'
+import { detectSectionFolder } from '../utils/SectionUtils.js'
 
 class SectionFactory {
   /**
@@ -22,6 +27,15 @@ class SectionFactory {
     // Scan package folder & categorize files
     const sectionFiles = await FileUtils.getFolderFilesRecursively(section.rootFolder)
     ComponentUtils.filterFiles(sectionFiles, section)
+
+    // Collate liquid content from all liquid files with the default folder/alphabetical order
+    logger.debug(`${section.liquidFiles.length} liquid file${section.liquidFiles.length > 1 ? 's' : ''} found`)
+    for (const liquidFile of section.liquidFiles) {
+      section.liquidCode += `\n${await readFile(liquidFile, FILE_ENCODING_OPTION)}`
+    }
+
+    section.renders = LiquidUtils.findRenders(section.liquidCode)
+    logger.debug(`${section.renders.length} render tag${section.renders.length > 1 ? 's' : ''} found`)
 
     return section
   }
