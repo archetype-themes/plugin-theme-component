a69aa79add0645889e63c30c9fda4bfd1fddb03a
Frédéric Léveillée <frederic@archetypethemes.co>
2022-08-09 17:39:46 -0400

    build section now uses the multiple libraries created in its glory.

--- bin/build-section.mjs
+++ bin/build-section.mjs
@@ -1,19 +1,19 @@
 #! /usr/bin/env node
-import { mkdir, readFile, rm, writeFile } from 'node:fs/promises'
+import { mkdir, rm, writeFile } from 'node:fs/promises'
 import { env, exit } from 'node:process'
-import { basename, extname } from 'path'
-import FileUtils, { FILE_ENCODING_OPTION } from '../utils/FileUtils.js'
+import { basename } from 'path'
+import FileUtils from '../utils/FileUtils.js'
 import SectionFactory from '../factory/SectionFactory.js'
 import logger from '../utils/Logger.js'
-import LiquidUtils from '../utils/LiquidUtils.js'
 import JavaScriptProcessor from '../processors/JavaScriptProcessor.js'
 import StylesProcessor from '../processors/StylesProcessor.js'
+import SnippetFactory from '../factory/SnippetFactory.js'
+import ComponentUtils from '../utils/ComponentUtils.js'
 
-const section = SectionFactory.createSection(env.npm_package_name)
+const section = await SectionFactory.createSection(env.npm_package_name)
 
 logger.info(`Building "${section.name}" section`)
 
-// Package components
 // Clean build folder
 try {
   await rm(section.buildFolder, { force: true, recursive: true })
@@ -23,55 +23,29 @@ try {
   exit(1)
 }
 
-// Scan package folder
-const sectionFiles = await FileUtils.getFolderFilesRecursively(section.rootFolder)
-
-// Categorize files for the build steps
-for (const sectionFile of sectionFiles) {
-  const extension = extname(sectionFile)
-
-  switch (extension) {
-    case '.css':
-    case '.less':
-    case '.sass':
-    case '.scss':
-      section.styleSheets.push(sectionFile)
-      break
-    case '.js':
-    case '.mjs':
-      section.jsFiles.push(sectionFile)
-      break
-    case '.liquid':
-      section.liquidFiles.push(sectionFile)
-      break
-    case '.json':
-      if (basename(sectionFile) === 'schema.json')
-        section.schemaFile = sectionFile
-      else if (basename(sectionFile).match(/^([a-z]{2})(-[a-z]{2})?(\.(default|schema)){0,2}\.json$/i))
-        section.localeFiles.push(sectionFile)
-      break
-    default:
-      logger.debug(`Ignoring ${FileUtils.convertToComponentRelativePath(sectionFile)}`)
-      break
-  }
-}
-
 // Abort if we have no liquid file
 if (section.liquidFiles.length === 0) {
   logger.error('No liquid file found - aborting build')
   exit(1)
 }
 
-// Collate liquid content from all liquid files with the default folder/alphabetical order
-let liquidCode = ''
+for (const render of section.renders) {
+  const snippet = await SnippetFactory.fromRender(render)
 
-logger.debug(`${section.liquidFiles.length} liquid file${section.liquidFiles.length > 1 ? 's' : ''} found`)
-for (const liquidFile of section.liquidFiles) {
-  liquidCode += `\n${await readFile(liquidFile, FILE_ENCODING_OPTION)}`
+  for (const snippetFile in section.snippetFiles) {
+
+    if (snippetFile === snippet.name) {
+      snippet.liquidFiles = [snippetFile]
+      break
+    }
+  }
+  if (snippet.liquidFiles.length === 0) {
+    const snippetFiles = await FileUtils.getFolderFilesRecursively(snippet.rootFolder)
+    ComponentUtils.filterFiles(snippetFiles, snippet)
+  }
+
+  render.snippet = snippet
 }
-
-const renders = LiquidUtils.findRenders(liquidCode)
-console.log(renders)
 
 // Create build/assets folder if we have any assets
 if (section.jsFiles.length > 0 || section.styleSheets.length > 0) {
@@ -125,12 +99,12 @@ if (section.localeFiles.length > 0) {
 // append section schema
 if (section.schemaFile) {
   logger.debug('Processing Schema file')
-  liquidCode += `\n{% schema %}\n${await FileUtils.readFileOrDie(section.schemaFile)}\n{% endschema %}`
+  section.liquidCode += `\n{% schema %}\n${await FileUtils.readFileOrDie(section.schemaFile)}\n{% endschema %}`
 }
 
 // create section liquid file
 logger.debug('Finalizing Liquid file')
 const liquidBuildFile = section.buildFolder + '/' + section.name + '.liquid'
-await writeFile(liquidBuildFile, liquidCode)
+await writeFile(liquidBuildFile, section.liquidCode)
 
 logger.info('Work Complete')
