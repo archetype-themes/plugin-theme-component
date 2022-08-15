e4723e9719d460ad3ebddd631c7ab2e4378a0cf4
Frédéric Léveillée <frederic@archetypethemes.co>
2022-08-14 23:31:08 -0400

    Added code for the Generation of an inline Liquid Snippet

--- utils/LiquidUtils.js
+++ utils/LiquidUtils.js
@@ -1,4 +1,5 @@
 import RenderFactory from '../factory/RenderFactory.js'
+import FileUtils from './FileUtils.js'
 
 class LiquidUtils {
 
@@ -17,7 +18,34 @@ class LiquidUtils {
     }
     return renders
   }
-  
+
+  /**
+   *
+   * @param {Render} render
+   * @returns {Promise<string>}
+   */
+  static async getRenderSnippetInlineLiquidCode (render) {
+    let snippetLiquidCode
+    if (render.snippet.liquidCode) {
+      snippetLiquidCode = render.snippet.liquidCode
+    } else {
+      snippetLiquidCode = await FileUtils.mergeFileContents(render.snippet.files.liquidFiles)
+    }
+
+    // Process "With" clause variable
+    if (render.hasWithClause() && render.clauseSourceVariable !== render.clauseTargetVariable) {
+      snippetLiquidCode = `{% assign ${render.clauseTargetVariable} = ${render.clauseSourceVariable} %}\n${snippetLiquidCode}`
+    }
+
+    // Process additional variables
+    for (const renderVariable in render.variables) {
+      if (renderVariable !== render.variables[renderVariable]) {
+        snippetLiquidCode = `{% assign ${renderVariable} = ${render.variables[renderVariable]} %}\n${snippetLiquidCode}`
+      }
+    }
+
+    return snippetLiquidCode
+  }
 }
 
 export default LiquidUtils
