e790f0673cda67fb007a93682ed94956ce1e3b86
Frédéric Léveillée <frederic@archetypethemes.co>
2022-09-23 16:16:51 -0400

    ESBUILD: Dropping IIFE mode for CJS

--- src/processors/EsbuildProcessor.js
+++ src/processors/EsbuildProcessor.js
@@ -22,7 +22,8 @@ class EsbuildProcessor {
       //drop: ['console'], // TODO: Check with Team to see if we want to use this feature for bundled code or not.
       entryPoints: [mainJavaScriptFile],
       // metafile: true,  // result = build(options); logger.debug(result.metafile);return result
-      //minify: true,  // TODO: Check with Team to see if we want to use this feature for bundled code or not.
+      //minify: true,  // Could be an option in the archie config
+      format: 'cjs', // Defaults to iife on browser platform, but this wraps the code inside an immediately-invoked function expression
       outfile: outputFile,
       platform: 'browser',
       sourcemap: true,
@@ -48,7 +49,7 @@ class EsbuildProcessor {
       bundle: true,
       charset: 'utf8',
       //metafile: true,
-      //minify: true, // TODO: Check with Team to see if we want to use this feature for bundled code or not.
+      //minify: true,
       platform: 'browser',
       plugins: [sassPlugin(), lessLoader()],
       sourcemap: true,
