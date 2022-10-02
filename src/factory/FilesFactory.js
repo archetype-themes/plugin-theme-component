7fa6e9d8365d2ec230581dd99305afb1df159191
Frédéric Léveillée <frederic@archetypethemes.co>
2022-10-01 22:10:03 -0400

    Files Factory modified to remove initialization of variables from FileUtils filterFiles method.

--- src/factory/FilesFactory.js
+++ src/factory/FilesFactory.js
@@ -2,6 +2,8 @@ import FileUtils from '../utils/FileUtils.js'
 import ComponentUtils from '../utils/ComponentUtils.js'
 import SectionFiles from '../models/SectionFiles.js'
 import SnippetFiles from '../models/SnippetFiles.js'
+import JavaScriptProcessor from '../processors/JavaScriptProcessor.js'
+import StylesProcessor from '../processors/StylesProcessor.js'
 
 class FilesFactory {
   /**
@@ -13,6 +15,14 @@ class FilesFactory {
     const files = await FileUtils.getFolderFilesRecursively(folder)
     const sectionFiles = new SectionFiles()
     ComponentUtils.filterFiles(files, sectionFiles)
+
+    if (sectionFiles.javascriptFiles.length > 0) {
+      sectionFiles.javascriptIndex = JavaScriptProcessor.getMainJavascriptFile(sectionFiles.javascriptFiles)
+    }
+
+    if (sectionFiles.stylesheets.length > 0) {
+      sectionFiles.mainStylesheet = StylesProcessor.getMainStyleSheet(sectionFiles.stylesheets)
+    }
 
     return sectionFiles
   }
@@ -26,6 +36,14 @@ class FilesFactory {
     const files = await FileUtils.getFolderFilesRecursively(folder)
     const snippetFiles = new SnippetFiles()
     ComponentUtils.filterFiles(files, snippetFiles)
+
+    if (snippetFiles.javascriptFiles.length > 0) {
+      snippetFiles.javascriptIndex = JavaScriptProcessor.getMainJavascriptFile(snippetFiles.javascriptFiles)
+    }
+
+    if (snippetFiles.stylesheets.length > 0) {
+      snippetFiles.mainStylesheet = StylesProcessor.getMainStyleSheet(snippetFiles.stylesheets)
+    }
 
     return snippetFiles
   }
