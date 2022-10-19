361915e4e217110be2d5ffdb971cfe8d810e3f95
Frédéric Léveillée <frederic@archetypethemes.co>
2022-10-19 15:53:38 -0400

    Added TODO note

--- src/watchers/CollectionWatcher.js
+++ src/watchers/CollectionWatcher.js
@@ -4,6 +4,7 @@ class CollectionWatcher {
    * @param {Collection} collection
    */
   static watch (collection) {
+    // TODO: Use chokidar here!
     throw new Error('Not Implemented Yet')
   }
 }
