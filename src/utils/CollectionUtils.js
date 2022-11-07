3f2f8eb16dbd7ddb63e74a0fda888a968fe3bbf2
Frédéric Léveillée <frederic@archetypethemes.co>
2022-11-07 13:32:08 -0500

    Added getWatchFolders to CollectionUtils.js

--- src/utils/CollectionUtils.js
+++ src/utils/CollectionUtils.js
@@ -1,6 +1,7 @@
 import { access, readdir } from 'node:fs/promises'
-import { join } from 'path'
+import path from 'path'
 import { constants } from 'node:fs'
+import ComponentUtils from './ComponentUtils.js'
 
 class CollectionUtils {
   /**
@@ -13,12 +14,32 @@ class CollectionUtils {
     for (const entry of entries) {
       if (entry.isDirectory()) {
         try {
-          const sectionFolder = join(collection.sectionsFolder, entry.name)
+          const sectionFolder = path.join(collection.sectionsFolder, entry.name)
           await access(sectionFolder + '/package.json', constants.R_OK)
           collection.sectionNames.push(entry.name)
         } catch {}
       }
     }
+  }
+
+  /**
+   * Get Watch Folders for a Collection
+   * @param collection
+   * @return {*[]}
+   */
+  static getWatchFolders (collection) {
+    let watchFolders = []
+
+    for (const section of collection.sections) {
+      watchFolders.push(section.rootFolder)
+      for (const snippetRootFolder of ComponentUtils.getSnippetRootFoldersFromRenders(section.renders)) {
+        if (!watchFolders.includes(snippetRootFolder)) {
+          watchFolders.push(snippetRootFolder)
+        }
+      }
+    }
+
+    return watchFolders.map(folder => path.join(folder, 'src'))
   }
 }
 
