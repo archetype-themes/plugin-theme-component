f7a73fc46acda66e9a10372573825ac8d8d49782
Frédéric Léveillée <frederic@archetypethemes.co>
2022-11-07 16:53:16 -0500

    - Watcher restructured to be able to return a watcher instance so that it can be used to stop it if there are file changes. - BuildCommand.js implements the new watcher and now dynamically watches the appropriate folders.

--- src/utils/Watcher.js
+++ src/utils/Watcher.js
@@ -1,16 +1,39 @@
 import chokidar from 'chokidar'
+import logger from './Logger.js'
 
 class Watcher {
 
-  static watch (targets, action, rootFolder) {
-    const watcher = chokidar.watch(targets, {
+  /**
+   * Watch target files and folders
+   * @param {string[]} targets target paths
+   * @param {string} [rootFolder] Use if target paths are relative
+   * @return {FSWatcher}
+   */
+  static getWatcher (targets, rootFolder) {
+    const watchOptions = {
       awaitWriteFinish: {
         pollInterval: 20, stabilityThreshold: 60
-      }, cwd: rootFolder, ignoreInitial: true
-    })
+      }, ignoreInitial: true
+    }
 
-    watcher.on('all', action)
+    if (rootFolder) {
+      watchOptions.cwd = rootFolder
+    }
+    logger.debug('Chokidar will watch the following files & folders:')
+    logger.debug(targets)
+    return chokidar.watch(targets, watchOptions)
   }
+
+  /**
+   * Apply watch action to all files
+   * @param {FSWatcher} watcher
+   * @param {function} action
+   * @return {FSWatcher}
+   */
+  static watch (watcher, action) {
+    return watcher.on('all', action)
+  }
+
 }
 
 export default Watcher
