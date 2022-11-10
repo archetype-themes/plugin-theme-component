4737949021fc52e50f2425272371edc2ecc074ef
Frédéric Léveillée <frederic@archetypethemes.co>
2022-11-10 12:27:28 -0500

    SnippetFactory.js now handles Render elements as source

--- src/factory/SnippetFactory.js
+++ src/factory/SnippetFactory.js
@@ -1,28 +1,34 @@
+// NodeJS imports
+import path from 'path'
+
+// Archie module imports
+import BuildFactory from './BuildFactory.js'
 import FilesFactory from './FilesFactory.js'
 import Snippet from '../models/Snippet.js'
-import logger from '../utils/Logger.js'
-import FileUtils from '../utils/FileUtils.js'
-import ComponentUtils from '../utils/ComponentUtils.js'
-import BuildFactory from './BuildFactory.js'
 import SnippetFiles from '../models/SnippetFiles.js'
+import ComponentUtils from '../utils/ComponentUtils.js'
+import FileUtils from '../utils/FileUtils.js'
+import logger from '../utils/Logger.js'
 
 class SnippetFactory {
   /**
    * Build a full Snippet from its name by locating and scanning its home folder
    * @param {string} snippetName
+   * @param {string} snippetsPath
    * @returns {Promise<Snippet>}
    */
-  static async externalToSection (snippetName) {
+  static async externalToSection (snippetName, snippetsPath) {
     const snippet = new Snippet()
     snippet.name = snippetName
 
     // Set Snippet folders
-    snippet.rootFolder = await ComponentUtils.getValidRootFolder(snippet)
+    snippet.rootFolder = path.join(snippetsPath, snippet.name)
     snippet.build = BuildFactory.fromSnippet(snippet)
     snippet.files = await FilesFactory.fromSnippetFolder(snippet.rootFolder)
 
     // Prepare Snippet liquid code
-    logger.debug(`${snippet.name}: ${snippet.files.liquidFiles.length} liquid file${snippet.files.liquidFiles.length > 1 ? 's' : ''} found`)
+    const pluralForm = snippet.files.liquidFiles.length > 1 ? 's' : ''
+    logger.debug(`${snippet.name}: ${snippet.files.liquidFiles.length} liquid file${pluralForm} found`)
     snippet.liquidCode = await FileUtils.getMergedFilesContent(snippet.files.liquidFiles)
 
     // Prepare Snippet Schema
@@ -52,6 +58,45 @@ class SnippetFactory {
     snippet.liquidCode = await FileUtils.getMergedFilesContent(snippet.files.liquidFiles)
 
     return snippet
+  }
+
+  /**
+   *
+   * @param {Render} render
+   * @param {string[]} sectionSnippetFiles
+   * @param {string} snippetsPath
+   * @return {Promise<Snippet>}
+   */
+  static async fromRender (render, sectionSnippetFiles, snippetsPath) {
+
+    // Look within the section's local snippets first
+    for (const snippetFile of sectionSnippetFiles) {
+      if (render.snippetName === path.parse(snippetFile).name) {
+        return await this.internalToSection(render.snippetName, snippetFile)
+      }
+    }
+
+    return this.externalToSection(render.snippetName, snippetsPath)
+  }
+
+  /**
+   * Create Snippets from Render items
+   * @param {Render[]} renders
+   * @param {string[]} componentInternalSnippetFiles
+   * @param {string} snippetsPath
+   * @return {Promise<Render[]>}
+   */
+  static async fromRenders (renders, componentInternalSnippetFiles, snippetsPath) {
+    const snippetCache = []
+    for (const render of renders) {
+
+      // Make sure snippets are processed only once.
+      if (!snippetCache[render.snippetName]) {
+        snippetCache[render.snippetName] = await this.fromRender(render, componentInternalSnippetFiles, snippetsPath)
+      }
+      render.snippet = snippetCache[render.snippetName]
+    }
+    return Promise.resolve(renders)
   }
 
 }
