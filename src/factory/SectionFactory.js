578f2ae7ab1c06245725fc7de935a992ee2d4795
Frédéric Léveillée <frederic@archetypethemes.co>
2022-12-22 23:15:04 -0500

    SectionFactory.js and SnippetFactory.js reviewed and corrected. - SchemaLocales are now independent of Locales load (as it should have been from the start) - Locales are loaded in schema.locales directly instead of duplicating it in a distinct locales property. - Improved JS comments - Single file Snippets now check for further render tags

--- src/factory/SectionFactory.js
+++ src/factory/SectionFactory.js
@@ -1,10 +1,11 @@
 // Node JS Internal imports
 import { env } from 'node:process'
 import path from 'path'
+
 // External Node JS Modules
 import merge from 'deepmerge'
+
 // Archie Internal JS imports
-import BuildFactory from './BuildFactory.js'
 import FilesFactory from './FilesFactory.js'
 import RenderFactory from './RenderFactory.js'
 import SnippetFactory from './SnippetFactory.js'
@@ -12,6 +13,7 @@ import ArchieNodeConfig from '../cli/models/ArchieNodeConfig.js'
 import FileAccessError from '../errors/FileAccessError.js'
 import Collection from '../models/Collection.js'
 import Section from '../models/Section.js'
+import SectionSchema from '../models/SectionSchema.js'
 import ComponentUtils from '../utils/ComponentUtils.js'
 import FileUtils from '../utils/FileUtils.js'
 import logger from '../utils/Logger.js'
@@ -20,15 +22,15 @@ class SectionFactory {
 
   /**
    * Builds a new Section and sets its basic parameters
-   * @param {string} name
-   * @param {module:models/Collection} [collection] Parent Collection
+   * @param {string} name - Section name
+   * @param {module:models/Collection} [collection] - Parent Collection
    * @returns {Promise<Section>}
    */
   static async fromName (name, collection) {
     const section = new Section()
     section.name = name
 
-    // Set Section folders
+    // Set root folder
     if (collection && collection.rootFolder) {
       section.rootFolder = path.join(collection.rootFolder, Collection.SECTIONS_SUB_FOLDER, section.name)
     } else if (ArchieNodeConfig.isCollection()) {
@@ -37,45 +39,55 @@ class SectionFactory {
       section.rootFolder = path.dirname(env.npm_package_json)
     }
 
+    // Validation: Make sure that the root folder is readable
     if (!await FileUtils.isReadable(section.rootFolder)) {
       logger.error(`Section Factory Abort: ${section.name} was not found at any expected location: "${section.rootFolder}".`)
       throw new FileAccessError(`Unable to access the "${section.name}" section on disk. Tips: Is it spelled properly in your archie config? Is the collection installed?`)
     }
 
-    // Generate build elements
-    section.build = BuildFactory.fromSection(section)
-    // Find section files
+    // Create Section Files model
     section.files = await FilesFactory.fromSectionFolder(section.rootFolder)
 
-    // Abort if no liquid file was found
+    // Validation: Make sure that a liquid file was found
     if (section.files.liquidFiles.length === 0) {
       throw new FileAccessError(`Section Factory: No liquid files file found for the "${section.name}" section`)
     }
 
-    // Load liquid code from files
+    // Load Liquid Code
     const pluralForm = section.files.liquidFiles.length > 1 ? 's' : ''
     logger.debug(`${section.name}: ${section.files.liquidFiles.length} liquid file${pluralForm} found`)
     section.liquidCode = await FileUtils.getMergedFilesContent(section.files.liquidFiles)
 
-    // Load Schema file content
+    // Load Schema
     if (section.files.schemaFile) {
-      section.schema = JSON.parse(await FileUtils.getFileContents(section.files.schemaFile))
-      // Copy locales content from schema file
+      section.schema = new SectionSchema()
+      const sectionSchemaJson = JSON.parse(await FileUtils.getFileContents(section.files.schemaFile))
+      section.schema = Object.assign(section.schema, sectionSchemaJson)
+    }
+
+    // Load Locales
+    if (section.files.localeFiles && section.files.localeFiles.length > 0) {
+      const locales = await ComponentUtils.parseLocaleFilesContent(section.files.localeFiles)
+      if (!section.schema) {
+        section.schema = new SectionSchema()
+      }
       if (section.schema.locales) {
-        section.locales = section.schema.locales
+        section.schema.locales = merge(section.schema.locales, locales)
+      } else {
+        section.schema.locales = locales
       }
     }
 
-    // Load locale files content
-    if (section.files.localeFiles && section.files.localeFiles.length > 0) {
-      section.locales = merge(section.locales, await ComponentUtils.parseLocaleFilesContent(section.files.localeFiles))
+    // Load Schema Locales
+    if (section.files.schemaLocaleFiles && section.files.schemaLocaleFiles.length > 0) {
       section.schemaLocales = await ComponentUtils.parseLocaleFilesContent(section.files.schemaLocaleFiles)
     }
 
-    // Create Render Models from render tags in  Liquid Code
-    const snippetsPath = path.join(section.rootFolder, '../../', Collection.SNIPPETS_SUB_FOLDER)
+    // Create Renders
     section.renders = RenderFactory.fromLiquidCode(section.liquidCode)
-    // Create Child Snippet Models Within Render Models
+
+    // Create Snippets Recursively
+    const snippetsPath = path.join(section.rootFolder, '../../', Collection.SNIPPETS_SUB_FOLDER)
     section.renders = await SnippetFactory.fromRenders(section.renders, section.files.snippetFiles, snippetsPath)
 
     return section
--- src/factory/SnippetFactory.js
+++ src/factory/SnippetFactory.js
@@ -12,59 +12,71 @@ import logger from '../utils/Logger.js'
 import FileAccessError from '../errors/FileAccessError.js'
 import merge from 'deepmerge'
 import RenderFactory from './RenderFactory.js'
+import SectionSchema from '../models/SectionSchema.js'
 
 class SnippetFactory {
   /**
    * Build a full Snippet from its name by locating and scanning its home folder
-   * @param {string} snippetName
-   * @param {string} snippetsPath
+   * @param {string} snippetName - Snippet name
+   * @param {string} snippetsPath - Collection's snippets folder
    * @returns {Promise<Snippet>}
    */
   static async fromSnippetFolder (snippetName, snippetsPath) {
     const snippet = new Snippet()
     snippet.name = snippetName
 
-    // Set Snippet folders
+    // Set root folder
     snippet.rootFolder = path.join(snippetsPath, snippet.name)
 
+    // Validation: Make sure that the root folder is readable
     if (!await FileUtils.isReadable(snippet.rootFolder)) {
       logger.error(`Snippet Factory Abort: ${snippet.name} was not found at any expected location: "${snippet.rootFolder}".`)
       throw new FileAccessError(`Unable to access the "${snippet.name}" section on disk. Tips: Is it spelled properly? Is the collection installed?`)
     }
 
-    // Generate build elements
-    snippet.build = BuildFactory.fromSnippet(snippet)
-    // Find snippet files
+    // Create Snippet Files Model
     snippet.files = await FilesFactory.fromSnippetFolder(snippet.rootFolder)
 
-    // Abort if no liquid file was found
+    // Validation: Make sure that a liquid file was founds
     if (snippet.files.liquidFiles.length === 0) {
       throw new FileAccessError(`Snippet Factory: No liquid files file found for the "${snippet.name}" snippet`)
     }
 
-    // Load liquid code from files
+    // Load Liquid Code
     const pluralForm = snippet.files.liquidFiles.length > 1 ? 's' : ''
     logger.debug(`${snippet.name}: ${snippet.files.liquidFiles.length} liquid file${pluralForm} found`)
     snippet.liquidCode = await FileUtils.getMergedFilesContent(snippet.files.liquidFiles)
 
-    // Load Schema file content
+    // Load Schema
     if (snippet.files.schemaFile) {
-      snippet.schema = JSON.parse(await FileUtils.getFileContents(snippet.files.schemaFile))
-      // Copy locales content content from schema file
+      snippet.schema = new SectionSchema()
+      const snippetSchemaJson = JSON.parse(await FileUtils.getFileContents(snippet.files.schemaFile))
+      snippet.schema = Object.assign(snippet.schema, snippetSchemaJson)
+    }
+
+    // Load Locales
+    if (snippet.files.localeFiles && snippet.files.localeFiles.length > 0) {
+      const locales = await ComponentUtils.parseLocaleFilesContent(snippet.files.schemaLocaleFiles)
+
+      if (!snippet.schema) {
+        snippet.schema = new SectionSchema()
+      }
       if (snippet.schema.locales) {
-        snippet.locales = snippet.schema.locales
+        snippet.schema.locales = merge(snippet.schema.locales, locales)
+      } else {
+        snippet.schema.locales = locales
       }
     }
 
-    // Load locale files content
-    if (snippet.files.localeFiles && snippet.files.localeFiles.length > 0) {
-      snippet.locales = merge(snippet.locales, await ComponentUtils.parseLocaleFilesContent(snippet.files.localeFiles))
+    // Load Schema Locales
+    if (snippet.files.schemaLocaleFiles && snippet.files.schemaLocaleFiles.length > 0) {
       snippet.schemaLocales = await ComponentUtils.parseLocaleFilesContent(snippet.files.schemaLocaleFiles)
     }
 
-    // Create Render Models form Liquid Code
+    // Create Renders
     snippet.renders = RenderFactory.fromLiquidCode(snippet.liquidCode, snippet.name)
-    // Create Child Snippet Models Within Render Models
+
+    // Create Snippets Recursively
     snippet.renders = await SnippetFactory.fromRenders(snippet.renders, snippet.files.snippetFiles, snippetsPath)
 
     return snippet
@@ -72,25 +84,31 @@ class SnippetFactory {
 
   /**
    * Builds a Snippet internal to a Section, self-contained within a single liquid file
-   * @param {string} snippetName
-   * @param {string} snippetFile
+   * @param {string} snippetName - Snippet name
+   * @param {string} snippetFile - Snippet file
+   * @param {string} snippetsPath - Collection's snippets folder
    * @returns {Promise<Snippet>}
    */
-  static async fromSingleFile (snippetName, snippetFile) {
+  static async fromSingleFile (snippetName, snippetFile, snippetsPath) {
     const snippet = new Snippet()
     snippet.name = snippetName
     snippet.files = new SnippetFiles()
     snippet.files.liquidFiles = [snippetFile]
-    snippet.liquidCode = await FileUtils.getMergedFilesContent(snippet.files.liquidFiles)
+    snippet.liquidCode = await FileUtils.getFileContents(snippetFile)
+
+    // Non-Recursively Create Renders
+    snippet.renders = RenderFactory.fromLiquidCode(snippet.liquidCode, snippet.name)
+    // Create Snippets Within Renders
+    snippet.renders = await SnippetFactory.fromRenders(snippet.renders, snippet.files.snippetFiles, snippetsPath)
 
     return snippet
   }
 
   /**
    *
-   * @param {Render} render
+   * @param {Render} render - Render model
    * @param {string[]} componentInternalSnippetFiles
-   * @param {string} snippetsPath
+   * @param {string} snippetsPath - Collection's snippets folder
    * @return {Promise<Snippet>}
    */
   static async fromRender (render, componentInternalSnippetFiles, snippetsPath) {
@@ -98,7 +116,7 @@ class SnippetFactory {
     // Look within the section's local snippets first
     for (const snippetFile of componentInternalSnippetFiles) {
       if (render.snippetName === path.parse(snippetFile).name) {
-        return await this.fromSingleFile(render.snippetName, snippetFile)
+        return await this.fromSingleFile(render.snippetName, snippetFile, snippetsPath)
       }
     }
 
@@ -107,15 +125,14 @@ class SnippetFactory {
 
   /**
    * Create Snippets from Render items
-   * @param {Render[]} renders
-   * @param {string[]} componentInternalSnippetFiles
-   * @param {string} snippetsPath
+   * @param {Render[]} renders - Render models array
+   * @param {string[]} componentInternalSnippetFiles - The parent Section or Snippet's internal one-file snippet files
+   * @param {string} snippetsPath - Collection's snippets folder
    * @return {Promise<Render[]>}
    */
   static async fromRenders (renders, componentInternalSnippetFiles, snippetsPath) {
     const snippetCache = []
     for (const render of renders) {
-
       // Make sure snippets are processed only once.
       if (!snippetCache[render.snippetName]) {
         snippetCache[render.snippetName] = await this.fromRender(render, componentInternalSnippetFiles, snippetsPath)
