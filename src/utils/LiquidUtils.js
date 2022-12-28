f7296ff4a5f4516e675a2c6a832fb21fd4b1df66
Frédéric Léveillée <frederic@archetypethemes.co>
2022-12-27 21:25:22 -0500

    LiquidUtils.js: added generation methods for JS and stylesheet inclusions.

--- src/utils/LiquidUtils.js
+++ src/utils/LiquidUtils.js
@@ -13,6 +13,32 @@ class LiquidUtils {
   }
 
   /**
+   * Generate Javascript File Reference for Shopify Liquid Code
+   * @param {string} javascriptFile - Javascript file basename
+   * @param {boolean} [async=true] - Optional asynchronous load of the javascript file, defaults to true
+   * @return {string}
+   */
+  static generateJavascriptFileReference (javascriptFile, async = true) {
+    if (async) {
+      return `<script src="{{ '${javascriptFile}' | asset_url }}" async></script>`
+    }
+    return `<script src="{{ '${javascriptFile}' | asset_url }}"></script>`
+  }
+
+  /**
+   * Generate Stylesheet Reference for Shopify Liquid Code
+   * @param {string} stylesheetName - stylesheet basename
+   * @param {boolean} [preload=false] - Optionally preload the stylesheet, defaults to false
+   * @return {string}
+   */
+  static generateStylesheetReference (stylesheetName, preload = false) {
+    if (preload) {
+      return `{{ '${stylesheetName}' | global_asset_url | stylesheet_tag: preload: ${preload} }}`
+    }
+    return `{{ '${stylesheetName}' | global_asset_url | stylesheet_tag }}`
+  }
+
+  /**
    * Get Snippet Inline Liquid Code
    * Adds appropriate variables when necessary ahead of the liquid code
    * @param {string} sourceLiquidCode
