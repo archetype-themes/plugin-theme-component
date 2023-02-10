f90a74767be669dd89d6aa07328a4cdb01154b87
Frédéric Léveillée <frederic@archetypethemes.co>
2023-02-10 12:06:12 -0500

    Install backups are now optional.

--- src/bin/archie.js
+++ src/bin/archie.js
@@ -9,24 +9,24 @@ import NodeUtils from '../utils/NodeUtils.js'
 import CLICommands from '../config/CLICommands.js'
 
 //Init ArchieSession & ArchieCLI
-let archieCLI
+let cliSession
 try {
-  await NodeConfigFactory.fromPackageJsonData(await NodeUtils.getPackageJsonData())
-  archieCLI = CLISessionFactory.fromCommandLineInput(NodeUtils.getArgs())
+  NodeConfigFactory.fromPackageJsonData(await NodeUtils.getPackageJsonData())
+  cliSession = CLISessionFactory.fromCommandLineInput(NodeUtils.getArgs())
 } catch (error) {
   NodeUtils.exitWithError(error)
 }
 
 try {
-  switch (archieCLI.command) {
+  switch (cliSession.command) {
     case CLICommands.BUILD_COMMAND_NAME:
-      await BuildCommand.execute(archieCLI.commandOption, archieCLI.targetComponentName, archieCLI.watchMode)
+      await BuildCommand.execute(cliSession.commandOption, cliSession.targetComponentName, cliSession.watchMode)
       break
     case CLICommands.CREATE_COMMAND_NAME:
-      await CreateCommand.execute(archieCLI.commandOption, archieCLI.targetComponentName)
+      await CreateCommand.execute(cliSession.commandOption, cliSession.targetComponentName)
       break
     case CLICommands.INSTALL_COMMAND_NAME:
-      await InstallCommand.execute(archieCLI.targetComponentName, archieCLI.watchMode)
+      await InstallCommand.execute(cliSession.targetComponentName, cliSession.backupMode, cliSession.watchMode)
       break
     // There is no need for a default case - "Invalid command" was already handled in ArchieCLIFactory call above
   }
