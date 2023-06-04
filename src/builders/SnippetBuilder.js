ccca75eedd45e90a7baea66a0890370f332a63cb
Frédéric Léveillée <frederic@archetypethemes.co>
2023-06-04 15:48:38 -0400

    Removing ocnsole.log test entry

--- src/builders/SnippetBuilder.js
+++ src/builders/SnippetBuilder.js
@@ -3,7 +3,6 @@ import SvgProcessor from '../processors/SvgProcessor.js'
 class SnippetBuilder {
   static async buildLiquid (snippetName, snippetLiquidCode) {
     if (snippetName.startsWith('icon-') || snippetName.endsWith('-svg') || snippetName.endsWith('.svg')) {
-      console.log('BEFORE', snippetLiquidCode)
       return SvgProcessor.buildSvg(snippetName, snippetLiquidCode)
     }
     return snippetLiquidCode
