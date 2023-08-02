1ac5fb182863037353ddf6f72970081f32288621
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-08-02 13:50:43 -0400

    Prototype bundling JS with Rollup
    which allows use of multiple entry points for a bundle

--- src/main/processors/javascript/EsbuildProcessor.js
+++ src/main/processors/javascript/EsbuildProcessor.js
@@ -1,17 +1,18 @@
 // NodeJS core modules
-import { join } from 'node:path'
+import { join, basename } from 'node:path'
 
 // External Modules
-import esbuild from 'esbuild'
+import { rollup } from 'rollup'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import multi from '@rollup/plugin-multi-entry'
+
 import merge from 'deepmerge'
 
 // Internal modules
 import FileUtils from '../../../utils/FileUtils.js'
 import logger from '../../../utils/Logger.js'
 
-// eslint-disable-next-line no-unused-vars
-const { build, BuildResult } = esbuild
-
+// TODO: rename class, behavior should not be tied to a specific bundler
 class EsbuildProcessor {
   /**
    * Build JavaScript files for a section or snippet
@@ -23,34 +24,36 @@ class EsbuildProcessor {
    */
   static async buildJavaScript (configFilePath, outputFile, mainJavaScriptFile, injectedFiles) {
     const defaultOptions = {
-      bundle: true,
-      charset: 'utf8',
-      // drop: ['console'],
-      entryPoints: [mainJavaScriptFile],
-      // metafile: true,  // result = build(options); logger.debug(result.metafile);return result
-      // minify: true,
-      format: 'cjs', // Defaults to iife on browser platform, but this wraps the code inside an immediately-invoked function expression
-      outfile: outputFile,
-      platform: 'browser',
-      sourcemap: true,
-      target: ['chrome58', 'firefox57', 'safari11', 'edge18']
+      input: [mainJavaScriptFile, ...injectedFiles],
+      output: {
+        dir: outputFile.substring(0, outputFile.lastIndexOf('/'))
+      },
+      plugins: [
+        // Teach Rollup how to find modules in the node_modules folder
+        nodeResolve(),
+        // Teach Rollup how to handle multiple entry points for the same bundle
+        multi({
+          entryFileName: basename(outputFile)
+        })
+      ]
     }
 
-    if (injectedFiles) {
-      defaultOptions.inject = injectedFiles
-    }
+    // TODO: config bundler, via rollup.config.js or archie.config.js from next
+    // const esbuildConfigFile = join(configFilePath, 'esbuild.config.js')
+    // logger.debug(`ESBuild Config file: ${esbuildConfigFile}`)
+    // if (await FileUtils.isReadable(esbuildConfigFile)) {
+    //   const configFileOptions = await import(esbuildConfigFile)
+    //   /** @type {Object} **/
+    //   const options = merge(defaultOptions, configFileOptions.default)
+    //   logger.debug('esbuild external config file found and processed')
+    //   return build(options)
+    // }
+    // logger.debug('No ESBuild external config file was found. Using the embedded Archie default esbuild config.')
+    // return build(defaultOptions)
 
-    const esbuildConfigFile = join(configFilePath, 'esbuild.config.js')
-    logger.debug(`ESBuild Config file: ${esbuildConfigFile}`)
-    if (await FileUtils.isReadable(esbuildConfigFile)) {
-      const configFileOptions = await import(esbuildConfigFile)
-      /** @type {Object} **/
-      const options = merge(defaultOptions, configFileOptions.default)
-      logger.debug('esbuild external config file found and processed')
-      return build(options)
-    }
-    logger.debug('No ESBuild external config file was found. Using the embedded Archie default esbuild config.')
-    return build(defaultOptions)
+    // TODO: catch exceptions
+    const bundle = await rollup(defaultOptions)
+    return bundle.write(defaultOptions.output)
   }
 }
 
