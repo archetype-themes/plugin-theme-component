caea4c29ea5ede1ffdea1d43dd5c2b945418c2dc
Frédéric Léveillée <fleveillee@gmail.com>
2023-09-11 15:51:16 -0400

    Renaming getPackageJsonData to getPackageManifest

--- src/cli/factories/NodeConfigFactory.js
+++ src/cli/factories/NodeConfigFactory.js
@@ -7,42 +7,42 @@ import logger from '../../utils/Logger.js'
 class NodeConfigFactory {
   /**
    * Init Archie Config
-   * @param {Object} packageJsonData
+   * @param {Object} packageManifest
    * @return {void}
    */
-  static fromPackageJsonData (packageJsonData) {
+  static fromPackageManifest (packageManifest) {
     // Validate and load Component Type
-    NodeConfig.componentType = this.#findComponentType(packageJsonData)
+    NodeConfig.componentType = this.#findComponentType(packageManifest)
 
     // Load Component Path, if available
-    if (packageJsonData.archie?.componentPath) {
-      NodeConfig.componentPath = packageJsonData.archie.componentPath
+    if (packageManifest.archie?.componentPath) {
+      NodeConfig.componentPath = packageManifest.archie.componentPath
     }
 
-    if (packageJsonData.archie?.embedLocales) {
-      NodeConfig.embedLocales = packageJsonData.archie.embedLocales
+    if (packageManifest.archie?.embedLocales) {
+      NodeConfig.embedLocales = packageManifest.archie.embedLocales
     }
 
     if (NodeConfig.isTheme()) {
-      NodeConfig.collections = this.#findCollections(packageJsonData)
+      NodeConfig.collections = this.#findCollections(packageManifest)
     }
   }
 
   /**
    * Get Component Type
-   * @param {Object} packageJsonData
+   * @param {Object} packageManifest
    * @return {string}
    */
-  static #findComponentType (packageJsonData) {
+  static #findComponentType (packageManifest) {
     /** @var {archie} packageJson.archie **/
-    if (!packageJsonData.archie?.componentType) {
+    if (!packageManifest.archie?.componentType) {
       throw new ConfigError(`Couldn't find archie.componentType value in package.json. Please create the variable and set it to either one of these: ${CLI.AVAILABLE_COMPONENT_TYPES.join('/')}`)
     }
 
-    const componentType = packageJsonData.archie.componentType.toLowerCase()
+    const componentType = packageManifest.archie.componentType.toLowerCase()
 
     if (!CLI.AVAILABLE_COMPONENT_TYPES.includes(componentType)) {
-      throw new ConfigError(`Invalid Archie Component Type: The value for archie.componentType from package.json must be changed to one of these: ${CLI.AVAILABLE_COMPONENT_TYPES.join('/')}, "${packageJsonData.archie.componentType}" is not an allowed value`)
+      throw new ConfigError(`Invalid Archie Component Type: The value for archie.componentType from package.json must be changed to one of these: ${CLI.AVAILABLE_COMPONENT_TYPES.join('/')}, "${packageManifest.archie.componentType}" is not an allowed value`)
     }
     logger.debug(`Component Type: "${componentType}"`)
 
@@ -51,12 +51,12 @@ class NodeConfigFactory {
 
   /**
    * Get Component Type
-   * @param {Object} packageJsonData
+   * @param {Object} packageManifest
    * @return {string[]}
    */
-  static #findCollections (packageJsonData) {
+  static #findCollections (packageManifest) {
     /** @var {archie} packageJson.archie **/
-    return packageJsonData.archie?.collections ? packageJsonData.archie.collections : []
+    return packageManifest.archie?.collections ? packageManifest.archie.collections : []
   }
 }
 
