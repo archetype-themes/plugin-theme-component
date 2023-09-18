38e3ef702b1063bf3d107d73ff6319060247e70a
Frédéric Léveillée <fleveillee@gmail.com>
2023-09-18 19:07:28 -0400

    CLISession wins, removing NodeConfig. having two session style libraries is a little bit of over-engineering. CLISession is now a global session, fully static. It is not passed down to methods anymore.

--- src/bin/archie.js
+++ src/bin/archie.js
@@ -1,32 +1,32 @@
 #!/usr/bin/env node
 
 import BuildCommand from '../cli/commands/BuildCommand.js'
+import CLISession from '../cli/models/CLISession.js'
+import CLICommands from '../config/CLICommands.js'
+import CLISessionFactory from '../cli/factories/CLISessionFactory.js'
 import CreateCommand from '../cli/commands/CreateCommand.js'
 import InstallCommand from '../cli/commands/InstallCommand.js'
-import CLISessionFactory from '../cli/factories/CLISessionFactory.js'
-import NodeConfigFactory from '../cli/factories/NodeConfigFactory.js'
 import NodeUtils from '../utils/NodeUtils.js'
-import CLICommands from '../config/CLICommands.js'
 
 // Init NodeConfig & CLISession
-let cliSession
+let packageManifest
 try {
-  NodeConfigFactory.fromPackageManifest(await NodeUtils.getPackageManifest())
-  cliSession = CLISessionFactory.fromCommandLineInput(NodeUtils.getArgs())
+  packageManifest = await NodeUtils.getPackageManifest()
+  CLISessionFactory.fromArgsAndManifest(NodeUtils.getArgs(), packageManifest)
 } catch (error) {
   NodeUtils.exitWithError(error)
 }
 
 try {
-  switch (cliSession.command) {
+  switch (CLISession.command) {
     case CLICommands.BUILD_COMMAND_NAME:
-      await BuildCommand.execute(cliSession.commandOption, cliSession.targetComponentName, cliSession.watchMode)
+      await BuildCommand.execute()
       break
     case CLICommands.CREATE_COMMAND_NAME:
-      await CreateCommand.execute(cliSession.commandOption, cliSession.targetComponentName)
+      await CreateCommand.execute(packageManifest)
       break
     case CLICommands.INSTALL_COMMAND_NAME:
-      await InstallCommand.execute(cliSession.targetComponentName, cliSession.watchMode)
+      await InstallCommand.execute()
       break
     // There is no need for a default case - "Invalid command" was already handled in ArchieCLIFactory call above
   }
