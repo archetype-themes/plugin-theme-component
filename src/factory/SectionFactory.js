5644677493c34ab0af912459a07e1f9b520df0ea
Frédéric Léveillée <fleveillee@gmail.com>
2023-09-22 20:46:33 -0400

    initializeSection method added and FromCollection method revised for the new build method.

--- src/factory/SectionFactory.js
+++ src/factory/SectionFactory.js
@@ -2,16 +2,16 @@
 import path from 'path'
 
 // Archie Internal JS imports
-import Session from '../models/static/Session.js'
 import Components from '../config/Components.js'
+import Section from '../models/Section.js'
+import SectionFiles from '../models/SectionFiles.js'
+import Session from '../models/static/Session.js'
 import ComponentFilesUtils from '../utils/ComponentFilesUtils.js'
 import LiquidUtils from '../utils/LiquidUtils.js'
 import LocaleUtils from '../utils/LocaleUtils.js'
 import logger from '../utils/Logger.js'
 import NodeUtils from '../utils/NodeUtils.js'
-import SectionFiles from '../models/SectionFiles.js'
 import SnippetFactory from './SnippetFactory.js'
-import Section from '../models/Section.js'
 
 class SectionFactory {
   /**
@@ -21,19 +21,32 @@ class SectionFactory {
    * @returns {Promise<Section>}
    */
   static async fromName (name, collectionRootFolder) {
-    const section = new Section()
+    let section = new Section()
     section.name = name
 
     // Set root folder
     if (collectionRootFolder) {
       section.rootFolder = path.join(collectionRootFolder, Components.SECTIONS_FOLDER_NAME, section.name)
-    } else if (Session.isCollection()) {
-      section.rootFolder =
-        path.join(NodeUtils.getPackageRootFolder(), Components.SECTIONS_FOLDER_NAME, section.name)
     } else if (Session.isSection()) {
       section.rootFolder = NodeUtils.getPackageRootFolder()
     }
 
+    section = this.initializeSection(section)
+
+    // Create Snippets Recursively
+    if (section.snippetNames.length) {
+      logger.info(`└─> Section ${section.name} has the following snippets: ${section.snippetNames.join(', ')} `)
+      const snippetsPath = path.join(section.rootFolder, '../../', Components.SNIPPETS_FOLDER_NAME)
+      section.snippets = await SnippetFactory.fromNames(section.snippetNames, snippetsPath, section.files.snippetFiles)
+    }
+  }
+
+  /**
+   *
+   * @param {Section} section
+   * @returns {Promise<Section>}
+   */
+  static async initializeSection (section) {
     // Index Section Files
     section.files = await ComponentFilesUtils.indexFiles(section.name, section.rootFolder, new SectionFiles())
 
@@ -60,31 +73,28 @@ class SectionFactory {
       section.settingsSchema = await ComponentFilesUtils.getSettingsSchema(section.files.settingsSchemaFile)
     }
 
-    // Create Snippets Recursively
-    const snippetNames = LiquidUtils.getSnippetNames(section.liquidCode)
-    if (snippetNames.length) {
-      logger.info(`└─> Section ${section.name} has the following snippets: ${snippetNames.join(', ')} `)
-      const snippetsPath = path.join(section.rootFolder, '../../', Components.SNIPPETS_FOLDER_NAME)
-      section.snippets = await SnippetFactory.fromNames(snippetNames, snippetsPath, section.files.snippetFiles)
-    }
+    section.snippetNames = LiquidUtils.getSnippetNames(section.liquidCode)
 
     return section
   }
 
   /**
    * Create Sections from a Collection
-   * @param {string[]} sectionNames
-   * @param {string} collectionRootFolder
+   * @param {Section[]} sections
+   * @param {string[]} [sectionNames]
    * @return {Promise<Section[]>}
    */
-  static async fromCollection (sectionNames, collectionRootFolder) {
-    const sections = []
+  static async fromCollection (sections, sectionNames) {
+    const filteredSections = []
+    const filterWithNames = !!(sectionNames && sectionNames.length)
     // Create sections
-    for (const sectionName of sectionNames) {
-      const section = await SectionFactory.fromName(sectionName, collectionRootFolder)
-      sections.push(section)
+    for (const section of sections) {
+      // Skip entry if we are filtering and that the section name is not present in the list
+      if (filterWithNames && !sectionNames.includes(section.name)) { continue }
+
+      filteredSections.push(await SectionFactory.initializeSection(section))
     }
-    return sections
+    return filteredSections
   }
 }
 
