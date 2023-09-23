8854c650e6d46f06e29c371c90f4386301a92027
Frédéric Léveillée <fleveillee@gmail.com>
2023-09-22 20:43:42 -0400

    CollectionFactory.js now gets components from package.json files

--- src/factory/CollectionFactory.js
+++ src/factory/CollectionFactory.js
@@ -1,10 +1,9 @@
+// Node.js imports
 // Archie imports
-import Session from '../models/static/Session.js'
-import NodeUtils from '../utils/NodeUtils.js'
 import Collection from '../models/Collection.js'
+import Session from '../models/static/Session.js'
 import CollectionUtils from '../utils/CollectionUtils.js'
-import FileAccessError from '../errors/FileAccessError.js'
-import SectionFactory from './SectionFactory.js'
+import FileUtils from '../utils/FileUtils.js'
 import logger from '../utils/Logger.js'
 
 class CollectionFactory {
@@ -23,31 +22,22 @@ class CollectionFactory {
     // Set folder names
     collection.rootFolder = await CollectionUtils.findRootFolder(collectionName)
 
-    if (Session.archieConfig.componentFolders) {
-      if (Array.isArray(Session.archieConfig.componentFolders)) {
-        collection.componentsFolders = Session.archieConfig.componentFolders
-      } else if (NodeUtils.isString(Session.archieConfig.componentFolders)) {
-        collection.componentsFolders = [Session.archieConfig.componentFolders]
-      }
-    } else {
-      collection.componentsFolders = await CollectionUtils.findComponentFolders(collection.rootFolder)
-    }
+    // Recursively find all package.json files
+    collection.packageJsonFiles = await FileUtils.searchFile(collection.rootFolder, 'package.json', true)
+
+    // Create Components and Sections from package.json files data
+    const [components, sections, snippets] = await CollectionUtils.findComponentsFromPackageJsonFiles(collection.packageJsonFiles)
+
+    collection.components = components
+    collection.sections = sections
+    collection.snippets = snippets
 
     // Get Section Names and create Sections
     if (componentNames && componentNames.length) {
       collection.sectionNames = componentNames
-    } else {
-      if (Session.isTheme()) {
-        logger.warn(`No component list found for the "${collectionName}" collection; all components will be installed.`)
-      }
-      try {
-        collection.sectionNames = await CollectionUtils.findComponentNames(collection.componentsFolders)
-      } catch (error) {
-        throw new FileAccessError(`Couldn't find or access components for the "${collectionName}" collection on disk. Please verify your install and file permissions.`)
-      }
+    } else if (Session.isTheme()) {
+      logger.warn(`No component list found for the "${collectionName}" collection; all components will be installed.`)
     }
-
-    collection.sections = await SectionFactory.fromCollection(collection.sectionNames, collection.rootFolder)
 
     return collection
   }
