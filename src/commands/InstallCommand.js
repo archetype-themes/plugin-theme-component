9aea931961a3a837a00c880895e70554f7aa5a8a
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-11-25 22:51:54 -0500

    Use the jsProcessor option in the install command

--- src/commands/InstallCommand.js
+++ src/commands/InstallCommand.js
@@ -1,5 +1,6 @@
 // Node imports
 import path from 'node:path'
+import {JS_PROCESSOR} from '../config/CLI.js'
 import ThemeFactory from '../factory/ThemeFactory.js'
 import Session from '../models/static/Session.js'
 import CollectionUtils from '../utils/CollectionUtils.js'
@@ -28,12 +29,13 @@ class InstallCommand {
     } else {
       collectionsList = Session.targets
     }
+    const jsProcessor = Session.jsProcessor ?? JS_PROCESSOR
 
     for (const [collectionName, componentNames] of Object.entries(collectionsList)) {
-      const collection = await InstallCommand.installOne(collectionName, componentNames)
+      const collection = await InstallCommand.installOne(collectionName, componentNames, jsProcessor)
 
       if (Session.watchMode) {
-        promises.push(this.watch(collection))
+        promises.push(this.watch(collection, jsProcessor))
       }
     }
 
@@ -44,9 +46,10 @@ class InstallCommand {
    * Install a Collection
    * @param {string} collectionName
    * @param {string[]} componentNames
+   * @param {string} jsProcessor
    * @return {Promise<module:models/Collection>}
    */
-  static async installOne (collectionName, componentNames) {
+  static async installOne (collectionName, componentNames, jsProcessor) {
     logger.info(`Building & Installing the ${collectionName} Collection.`)
     const startTime = Timer.getTimer()
 
@@ -54,7 +57,7 @@ class InstallCommand {
     const theme = ThemeFactory.fromThemeInstallCommand()
 
     // Build using the Build Command
-    const collection = await BuildCommand.buildCollection(collectionName, componentNames)
+    const collection = await BuildCommand.buildCollection(collectionName, componentNames, jsProcessor)
     await BuildCommand.deployCollection(collection)
     // Install and time it!
     logger.info(`Installing the ${collectionName} Collection for the ${theme.name} Theme.`)
@@ -69,36 +72,38 @@ class InstallCommand {
    * On Collection Watch Event
    * @param {string} collectionName
    * @param {string[]} componentNames
+   * @param {string} jsProcessor
    * @param {FSWatcher} watcher
    * @param event
    * @param eventPath
    * @return {Promise<module: models/Collection>}
    */
-  static async onCollectionWatchEvent (collectionName, componentNames, watcher, event, eventPath) {
+  static async onCollectionWatchEvent (collectionName, componentNames, jsProcessor, watcher, event, eventPath) {
     const filename = path.basename(eventPath)
     logger.debug(`Watcher Event: "${event}" on file: ${filename} detected`)
 
-    const collection = await InstallCommand.installOne(collectionName, componentNames)
+    const collection = await InstallCommand.installOne(collectionName, componentNames, jsProcessor)
 
     // The Watcher is restarted on any liquid file change.
     // This is useful if any render tags were added or removed, it will reset snippet watched folders.
     if (filename.endsWith('.liquid')) {
       await watcher.close()
-      return InstallCommand.watch(collection)
+      return InstallCommand.watch(collection, jsProcessor)
     }
   }
 
   /**
    * Build and Install Collection in Current Theme on File Change
    * @param {module:models/Collection} collection
+   * @param {string} jsProcessor
    * @return {Promise<module: models/Collection>}
    */
-  static async watch (collection) {
+  static async watch (collection, jsProcessor) {
     const ignorePatterns = CollectionUtils.getIgnorePatterns(collection)
 
     const watcher = Watcher.getWatcher(collection.rootFolder, ignorePatterns)
 
-    const onCollectionWatchEvent = this.onCollectionWatchEvent.bind(null, collection.name, collection.componentNames, watcher)
+    const onCollectionWatchEvent = this.onCollectionWatchEvent.bind(null, collection.name, collection.componentNames, jsProcessor, watcher)
     Watcher.watch(watcher, onCollectionWatchEvent)
   }
 }
