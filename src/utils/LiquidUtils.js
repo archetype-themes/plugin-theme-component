bb39b5591a3383e43b3aff66a836f50f4337927f
Frédéric Léveillée <fleveillee@gmail.com>
2023-12-06 17:20:52 -0500

    Removed unused methods

--- src/utils/LiquidUtils.js
+++ src/utils/LiquidUtils.js
@@ -1,7 +1,6 @@
 import liquidParser from '@shopify/liquid-html-parser'
 import FileUtils from './FileUtils.js'
 import InputFileError from '../errors/InputFileError.js'
-import SvgProcessor from '../processors/SvgProcessor.js'
 
 class LiquidUtils {
   /**
@@ -21,49 +20,6 @@ class LiquidUtils {
 
     // Remove duplicates
     return [...new Set(snippetNames)]
-  }
-
-  /**
-   * Generate Javascript File Reference for Shopify Liquid Code
-   * @param {string} javascriptFile - Javascript file basename
-   * @param {boolean} [async=true] - Optional asynchronous load of the javascript file, defaults to true
-   * @return {string}
-   */
-  static generateJavascriptFileReference (javascriptFile, async = true) {
-    if (async) {
-      return `<script src="{{ '${javascriptFile}' | asset_url }}" async></script>`
-    }
-    return `<script src="{{ '${javascriptFile}' | asset_url }}"></script>`
-  }
-
-  /**
-   * Generate Stylesheet Reference for Shopify Liquid Code
-   * @param {string} stylesheetName - stylesheet basename
-   * @param {boolean} [preload=false] - Optionally preload the stylesheet, defaults to false
-   * @return {string}
-   */
-  static generateStylesheetReference (stylesheetName, preload = false) {
-    if (preload) {
-      return `{{ '${stylesheetName}' | global_asset_url | stylesheet_tag: preload: ${preload} }}`
-    }
-    return `{{ '${stylesheetName}' | global_asset_url | stylesheet_tag }}`
-  }
-
-  /**
-   * Build Liquid Code
-   * @param {string} name
-   * @param {string} liquidCode
-   * @param{string} collectionRootFolder
-   * @return {Promise<string>}
-   */
-  static async buildLiquid (name, liquidCode, collectionRootFolder) {
-    let buildLiquidCode = liquidCode
-    // Process as SVG if applicable
-    if (name.startsWith('icon-') || name.endsWith('-svg') || name.endsWith('.svg')) {
-      buildLiquidCode = SvgProcessor.buildSvg(name, liquidCode, collectionRootFolder)
-    }
-
-    return buildLiquidCode
   }
 
   /**
