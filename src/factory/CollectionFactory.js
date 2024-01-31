76c2e4805f325026ef3fe379b3aea2d0ebd5c9f0
Frédéric Léveillée <fleveillee@gmail.com>
2024-01-30 20:58:02 -0500

    Adding an optional componentsource param to CollectionFactory.js Adding a shortcut method to CollectionFactory.js for collectionTomlEntries

--- src/factory/CollectionFactory.js
+++ src/factory/CollectionFactory.js
@@ -12,17 +12,18 @@ class CollectionFactory {
   /**
    * From Collection Build Script
    * @param {string} collectionName - Collection Name
-   * @param {string[]} componentNames - List of Components To Bundle With The Collection
+   * @param {string[]} [componentNames] - List of Components To Bundle With The Collection
+   * @param {string} [collectionSource] - Collection Source Path or URL
    * @return {Promise<module:models/Collection>}
    */
-  static async fromName (collectionName, componentNames) {
+  static async fromName (collectionName, componentNames, collectionSource) {
     const collection = new Collection()
 
     // Set Collection name
     collection.name = collectionName
 
     // Set Folder Names
-    collection.rootFolder = await CollectionUtils.findRootFolder(collectionName)
+    collection.rootFolder = collectionSource || await CollectionUtils.findRootFolder(collectionName)
 
     // Find .gitignore File
     const gitignoreFile = join(collection.rootFolder, '.gitignore')
@@ -37,13 +38,17 @@ class CollectionFactory {
     collection.components = CollectionUtils.findComponents(componentFolders)
 
     // Get Component Names and Create Them
-    if (componentNames && componentNames.length) {
+    if (componentNames?.length) {
       collection.componentNames = componentNames
     } else if (Session.isTheme()) {
       logger.warn(`No component list found for the "${collectionName}" collection; all components will be installed.`)
     }
 
     return collection
+  }
+
+  static async fromTomlEntry (collectionEntry) {
+    return this.fromName(collectionEntry[0], collectionEntry[1].components, collectionEntry[1].source)
   }
 }
 
