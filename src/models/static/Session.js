f1b8a0da5e58450c33c78111334d8a76e7b797df
Frédéric Léveillée <fleveillee@gmail.com>
2024-02-12 16:08:01 -0500

    Making properties public

--- src/models/static/Session.js
+++ src/models/static/Session.js
@@ -1,233 +1,48 @@
 import Components from '../../config/Components.js'
 
-/** @type {string} **/
-export const DEFAULT_DEV_THEME = 'https://github.com/archetype-themes/expanse.git'
-
-/** @type {string} **/
-export const DEFAULT_LOCALES_REPO = 'https://github.com/archetype-themes/locales.git'
-
 class Session {
-  /** @type {ComponentTomlConfig}  **/
-  static #config
+  /** @type {ComponentTomlConfig} Plugin TOML config  **/
+  static config
 
   /** @type {string} CLI Caller Component Type **/
-  static #callerType
-
-  /** @type {string}  **/
-  static #command
-
-  /** @type {string}  **/
-  static #devTheme
-
-  /**
-   * Indicates whether it is the first run.
-   *
-   * @type {boolean}
-   */
-  static #firstRun = true
-
-  /** @type {string}  **/
-  static #localesRepo
+  static callerType
 
   /** @type {boolean}  **/
   static #setupFiles
 
-  /** @type {boolean}  **/
-  static #syncMode = false
+  /** @type {string} Command Name  **/
+  static command
 
-  /** @type {string|Object}  **/
-  static #targets
+  /** @type {string} Dev Theme **/
+  static devTheme
 
-  /** @type {string}  **/
-  static #targetType
+  /** @type {boolean} Indicates whether it is the first run. **/
+  static firstRun = true
 
-  /** @type {boolean}  **/
-  static #watchMode = false
+  /** @type {string} Locales' source location **/
+  static localesRepo
 
-  /**
-   * Get CLI Config
-   * @returns {ComponentTomlConfig}
-   */
-  static get config () {
-    return this.#config
-  }
+  /** @type {boolean}  Copy components' setup files or not **/
+  static setupFiles
 
-  /**
-   * Set CLI Config
-   * @param {ComponentTomlConfig} value
-   */
-  static set config (value) {
-    this.#config = value
-  }
+  /** @type {boolean} Run "shopify theme dev" in parallel to sync files with a local dev **/
+  static syncMode = false
 
-  /**
-   * Get CLI Caller Component Type
-   * @returns {string}
-   */
-  static get callerType () {
-    return this.#callerType
-  }
+  /** @type {string|Object} target components or collection names **/
+  static targets
 
-  /**
-   * Set CLI Caller Component Type
-   * @param {string} value
-   */
-  static set callerType (value) {
-    this.#callerType = value
-  }
+  /** @type {string}  target type **/
+  static targetType
 
-  /**
-   * Get Command Name
-   * @return {string}
-   */
-  static get command () {
-    return this.#command
-  }
-
-  /**
-   * Set Command Name
-   * @param {string} value
-   */
-  static set command (value) {
-    this.#command = value
-  }
-
-  /**
-   * Get Dev Theme URL
-   * @returns {string}
-   */
-  static get devTheme () {
-    return this.#devTheme
-  }
-
-  /**
-   * Set Dev Theme URL
-   * @param {string} value
-   */
-  static set devTheme (value) {
-    this.#devTheme = value
-  }
-
-  /**
-   * Retrieves the value of the 'firstRun' property.
-   *
-   * @return {boolean} Indicates whether it is the first build
-   */
-  static get firstRun () {
-    return this.#firstRun
-  }
-
-  /**
-   * Sets the value of the firstRun property.
-   *
-   * @param {boolean} value - The new value for the firstRun property.
-   */
-  static set firstRun (value) {
-    this.#firstRun = value
-  }
-
-  /**
-   * Get localesRepo URL
-   * @returns {string}
-   */
-  static get localesRepo () {
-    return this.#localesRepo
-  }
-
-  /**
-   * Set localesRepo URL
-   * @param {string} value
-   */
-  static set localesRepo (value) {
-    this.#localesRepo = value
-  }
-
-  /**
-   * Get Setup Files Option
-   * @return {boolean}
-   */
-  static get setupFiles () {
-    return this.#setupFiles
-  }
-
-  /**
-   * Set Setup Files Option
-   * @param  {boolean} value
-   */
-  static set setupFiles (value) {
-    this.#setupFiles = value
-  }
-
-  /**
-   * Get Sync Mode
-   * @return {boolean}
-   */
-  static get syncMode () {
-    return this.#syncMode
-  }
-
-  /**
-   * Set Sync Mode
-   * @param {boolean} value
-   */
-  static set syncMode (value) {
-    this.#syncMode = value
-  }
-
-  /**
-   * Get Command Option
-   * @return {string}
-   */
-  static get targetType () {
-    return this.#targetType
-  }
-
-  /**
-   * Set Command Option
-   * @param {string} value
-   */
-  static set targetType (value) {
-    this.#targetType = value
-  }
-
-  /**
-   * Get Target Component Name
-   * @return {string|Object}
-   */
-  static get targets () {
-    return this.#targets
-  }
-
-  /**
-   * Set Target Component Name
-   * @param {string|Object} value
-   */
-  static set targets (value) {
-    this.#targets = value
-  }
-
-  /**
-   * Get Watch Mode
-   * @return {boolean}
-   */
-  static get watchMode () {
-    return this.#watchMode
-  }
-
-  /**
-   * Set Watch Mode
-   * @param {boolean} value
-   */
-  static set watchMode (value) {
-    this.#watchMode = value
-  }
+  /** @type {boolean} Watch for source file changes **/
+  static watchMode = false
 
   /**
    * Is The Component Type a Collection
    * @return {boolean}
    */
   static isCollection () {
-    return this.#callerType === Components.COLLECTION_TYPE_NAME
+    return this.callerType === Components.COLLECTION_TYPE_NAME
   }
 
   /**
@@ -235,7 +50,7 @@ class Session {
    * @return {boolean}
    */
   static isTheme () {
-    return this.#callerType === Components.THEME_TYPE_NAME
+    return this.callerType === Components.THEME_TYPE_NAME
   }
 }
 
