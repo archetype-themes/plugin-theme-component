4e9a5a58747b0670f8e1cf0eb8cb5a280f693911
Frédéric Léveillée <fleveillee@gmail.com>
2024-02-12 16:40:07 -0500

    Removing getters and setters

--- src/models/Collection.js
+++ src/models/Collection.js
@@ -1,190 +1,35 @@
 /** @module models/Collection */
 class Collection {
   /** @type {CollectionBuild} **/
-  #build
+  build
 
   /** @type {string} **/
-  #name
+  name
 
   /** @type {string} **/
-  #rootFolder
+  rootFolder
 
   /** @type {string} **/
-  #source
+  source
 
   /** @type {Component[]} **/
-  #components
+  components
 
   /** @type {string | undefined} **/
-  #gitIgnoreFile
+  gitIgnoreFile
 
   /** @type {string[]} **/
-  #packageJsonFiles
-
-  /** @type {string[]} **/
-  #componentNames
+  componentNames
 
   /** @type {Snippet[]} **/
-  #snippets
+  snippets
 
   /** @type {Map<string, string> | undefined} **/
-  #importMapEntries
+  importMapEntries
 
   constructor () {
     this.componentNames = []
     this.components = []
-  }
-
-  /**
-   * Get Collection Build
-   * @return {CollectionBuild}
-   */
-  get build () {
-    return this.#build
-  }
-
-  /**
-   * Set Collection Build
-   * @param {CollectionBuild} value
-   */
-  set build (value) {
-    this.#build = value
-  }
-
-  /**
-   * Get name
-   * @return {string}
-   */
-  get name () {
-    return this.#name
-  }
-
-  /**
-   * Set name
-   * @param {string} value
-   */
-  set name (value) {
-    this.#name = value
-  }
-
-  /**
-   * Get root folder
-   * @return {string}
-   */
-  get rootFolder () {
-    return this.#rootFolder
-  }
-
-  /**
-   * Set root folder
-   * @param {string} value
-   */
-  set rootFolder (value) {
-    this.#rootFolder = value
-  }
-
-  /**
-   * Get source
-   * @return {string|null}
-   */
-  get source () {
-    return this.#source
-  }
-
-  /**
-   * Set source
-   * @param {string|null} value
-   */
-  set source (value) {
-    this.#source = value
-  }
-
-  /**
-   * Get Components
-   * @returns {Component[]}
-   */
-  get components () {
-    return this.#components
-  }
-
-  /**
-   * Set Components
-   * @param {Component[]} value
-   */
-  set components (value) {
-    this.#components = value
-  }
-
-  /**
-   * Get gitignore file list
-   * @returns {string | undefined}
-   */
-  get gitIgnoreFile () {
-    return this.#gitIgnoreFile
-  }
-
-  /**
-   * Set gitignore file list
-   * @param {string | undefined} value
-   */
-  set gitIgnoreFile (value) {
-    this.#gitIgnoreFile = value
-  }
-
-  /**
-   * Get package.json file list
-   * @returns {string[]}
-   */
-  get packageJsonFiles () {
-    return this.#packageJsonFiles
-  }
-
-  /**
-   * Set package.json file list
-   * @param {string[]} value
-   */
-  set packageJsonFiles (value) {
-    this.#packageJsonFiles = value
-  }
-
-  /**
-   * Get component names
-   * @return {string[]}
-   */
-  get componentNames () {
-    return this.#componentNames
-  }
-
-  /**
-   * Set component names
-   * @param {string[]} value
-   */
-  set componentNames (value) {
-    this.#componentNames = value
-  }
-
-  /**
-   * Get Snippets
-   * @returns {Snippet[]}
-   */
-  get snippets () {
-    return this.#snippets
-  }
-
-  /**
-   * Set Snippets
-   * @param {Snippet[]} value
-   */
-  set snippets (value) {
-    this.#snippets = value
-  }
-
-  get importMapEntries () {
-    return this.#importMapEntries
-  }
-
-  set importMapEntries (value) {
-    this.#importMapEntries = value
   }
 
   /**
--- src/models/CollectionBuild.js
+++ src/models/CollectionBuild.js
@@ -2,211 +2,37 @@ import ComponentBuild from './ComponentBuild.js'
 
 class CollectionBuild extends ComponentBuild {
   /** @type {string} **/
-  #assetsFolder
+  assetsFolder
 
   /** @type {string} **/
-  #configFolder
+  configFolder
 
   /** @type {string} **/
-  #javascriptFile
+  javascriptFile
 
   /** @type {Object} **/
-  #locales
+  locales
 
   /** @type {string} **/
-  #localesFolder
+  localesFolder
 
   /** @type {string} **/
-  #rootFolder
+  rootFolder
 
   /** @type {string} **/
-  #sectionsFolder
+  sectionsFolder
 
   /** @type {string} **/
-  #importMapFile
+  importMapFile
 
   /** @type {string} **/
-  #snippetsFolder
+  snippetsFolder
 
   /** @type {string} **/
-  #styles
+  styles
 
   /** @type {string} **/
-  #stylesheet
-
-  /**
-   * Get Assets Build Folder Location
-   * @returns {string}
-   */
-  get assetsFolder () {
-    return this.#assetsFolder
-  }
-
-  /**
-   * Set Assets Build Folder Location
-   * @param {string} value
-   */
-  set assetsFolder (value) {
-    this.#assetsFolder = value
-  }
-
-  /**
-   * Get Config Folder
-   * @return {string}
-   */
-  get configFolder () {
-    return this.#configFolder
-  }
-
-  /**
-   * Set Config Folder
-   * @param {string} value
-   */
-  set configFolder (value) {
-    this.#configFolder = value
-  }
-
-  /**
-   * Get Javascript Build File Location
-   * @returns {string}
-   */
-  get javascriptFile () {
-    return this.#javascriptFile
-  }
-
-  /**
-   *Set Javascript Build File Location
-   * @param {string} value
-   */
-  set javascriptFile (value) {
-    this.#javascriptFile = value
-  }
-
-  /**
-   * Get Locales
-   * @return {Object}
-   */
-  get locales () {
-    return this.#locales
-  }
-
-  /**
-   * Set Locales
-   * @param {Object} value
-   */
-  set locales (value) {
-    this.#locales = value
-  }
-
-  /**
-   * Get Locales Build Folder Location
-   * @returns {string}
-   */
-  get localesFolder () {
-    return this.#localesFolder
-  }
-
-  /**
-   * Set Locales Build Folder Location
-   * @param {string} value
-   */
-  set localesFolder (value) {
-    this.#localesFolder = value
-  }
-
-  /**
-   * Get Root Build Folder Location
-   * @returns {string}
-   */
-  get rootFolder () {
-    return this.#rootFolder
-  }
-
-  /**
-   * Set Build Root Folder
-   * @param {string} value
-   */
-  set rootFolder (value) {
-    this.#rootFolder = value
-  }
-
-  /**
-   * Get Sections Folder
-   * @return {string}
-   */
-  get sectionsFolder () {
-    return this.#sectionsFolder
-  }
-
-  /**
-   * Set Sections Folder
-   * @param {string} value
-   */
-  set sectionsFolder (value) {
-    this.#sectionsFolder = value
-  }
-
-  /**
-   * Get Build Snippets Folder
-   * @returns {string}
-   */
-  get snippetsFolder () {
-    return this.#snippetsFolder
-  }
-
-  /**
-   * Set Build Snippets Folder
-   * @param {string} value
-   */
-  set snippetsFolder (value) {
-    this.#snippetsFolder = value
-  }
-
-  /**
-   * Get build styles
-   * @return {string}
-   */
-  get styles () {
-    return this.#styles
-  }
-
-  /**
-   * Set build styles
-   * @param {string} value
-   */
-  set styles (value) {
-    this.#styles = value
-  }
-
-  /**
-   * Get Stylesheet Build File Location
-   * @returns {string}
-   */
-  get stylesheet () {
-    return this.#stylesheet
-  }
-
-  /**
-   * Set Stylesheet Build File Location
-   * @param {string} value
-   */
-  set stylesheet (value) {
-    this.#stylesheet = value
-  }
-
-  /**
-   * Get Import Map File Location
-   */
-  get importMapFile () {
-    return this.#importMapFile
-  }
-
-  /**
-   * Set Import Map File Location
-   */
-  set importMapFile (value) {
-    this.#importMapFile = value
-  }
+  stylesheet
 }
 
 export default CollectionBuild
