2c206e76ae2b747930da66ba5677e8d72260642e
Frédéric Léveillée <fleveillee@gmail.com>
2024-02-12 16:30:30 -0500

    Added logEvent method to Watcher.js Utils

--- src/models/Component.js
+++ src/models/Component.js
@@ -1,27 +1,27 @@
 class Component {
   /** @type {string} **/
-  #name
+  name
 
   /** @type {string} **/
-  #rootFolder
+  rootFolder
 
   /** @type {ComponentFiles} **/
-  #files
+  files
 
   /** @type {ComponentBuild} **/
-  #build
+  build
 
   /** @type {string} **/
-  #liquidCode
+  liquidCode
 
-  /** @type {Object } **/
-  #locales = {}
+  /** @type {Object} **/
+  locales = {}
 
   /** @type {string[]} **/
-  #snippetNames
+  snippetNames
 
   /** @type {Snippet[]} **/
-  #snippets = []
+  snippets = []
 
   /**
    * Constructor
@@ -31,134 +31,6 @@ class Component {
   constructor (name, path) {
     if (name) this.name = name
     if (path) this.rootFolder = path
-  }
-
-  /**
-   * Get Name
-   * @returns {string}
-   */
-  get name () {
-    return this.#name
-  }
-
-  /**
-   * Set Name
-   * @param {string} value
-   */
-  set name (value) {
-    this.#name = value
-  }
-
-  /**
-   * Get Root Folder
-   * @returns {string}
-   */
-  get rootFolder () {
-    return this.#rootFolder
-  }
-
-  /**
-   * Set Root Folder
-   * @param {string} value
-   */
-  set rootFolder (value) {
-    this.#rootFolder = value
-  }
-
-  /**
-   * Get Files
-   * @returns {ComponentFiles}
-   */
-  get files () {
-    return this.#files
-  }
-
-  /**
-   * Set Files
-   * @param {ComponentFiles} value
-   */
-  set files (value) {
-    this.#files = value
-  }
-
-  /**
-   * Get Build
-   * @returns {ComponentBuild}
-   */
-  get build () {
-    return this.#build
-  }
-
-  /**
-   * Set Build
-   * @param {ComponentBuild} value
-   */
-  set build (value) {
-    this.#build = value
-  }
-
-  /**
-   * Get Liquid Code
-   * @returns {string}
-   */
-  get liquidCode () {
-    return this.#liquidCode
-  }
-
-  /**
-   * Liquid Code
-   * @param {string} value
-   */
-  set liquidCode (value) {
-    this.#liquidCode = value
-  }
-
-  /**
-   * Get Locales
-   * @return {Object}
-   */
-  get locales () {
-    return this.#locales
-  }
-
-  /**
-   * Set Locales
-   * @param {Object} value
-   */
-  set locales (value) {
-    this.#locales = value
-  }
-
-  /**
-   * Get Snippet Names
-   * @returns {string[]}
-   */
-  get snippetNames () {
-    return this.#snippetNames
-  }
-
-  /**
-   * Set Snippet Names
-   * @param {string[]}value
-   */
-  set snippetNames (value) {
-    this.#snippetNames = value
-  }
-
-  /**
-   * Get Snippets
-   * @returns {Snippet[]}
-   */
-  get snippets () {
-    return this.#snippets
-  }
-
-  /**
-   * Set Snippets
-   * @param {Snippet[]} value
-   */
-  set snippets (value) {
-    this.#snippets = value
   }
 
   isSvg () {
