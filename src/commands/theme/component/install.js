8f3e2171a7d0dafbfdf06e7aca4ee966c3ee64e0
Frédéric Léveillée <fleveillee@gmail.com>
2024-02-12 11:24:15 -0500

    Moved command to reflect default oclif setup

---
+++ src/commands/theme/component/install.js
@@ -0,0 +1,15 @@
+import InstallCommand from '../../InstallCommand.js'
+import { BaseCommand } from '../../../../plugin/baseCommand.js'
+import { getTomlConfig } from '../../../utils/TomlUtils.js'
+import { sessionFactory } from '../../../factory/SessionFactory.js'
+
+export default class Install extends BaseCommand {
+  static description = 'Install a collection of components'
+
+  async run () {
+    const tomlConfig = await getTomlConfig()
+    sessionFactory(this.id, tomlConfig)
+
+    return InstallCommand.execute()
+  }
+}
