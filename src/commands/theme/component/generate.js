98bd9f8497a2c56a0fe20163d7b1dc8917783e1f
Frédéric Léveillée <fleveillee@gmail.com>
2024-02-21 15:16:33 -0500

    generate handles multiple components creation

--- src/commands/theme/component/generate.js
+++ src/commands/theme/component/generate.js
@@ -1,5 +1,4 @@
 // Node.js Imports
-import { exec } from 'node:child_process'
 import { access, constants, mkdir } from 'node:fs/promises'
 import { join } from 'node:path'
 import { Args } from '@oclif/core'
@@ -14,15 +13,16 @@ import {
   getPackageRootFolder,
   getPackageScope
 } from '../../../utils/NodeUtils.js'
-import Components from '../../../config/Components.js'
+import { COMPONENTS_FOLDER } from '../../../config/Components.js'
 import FileAccessError from '../../../errors/FileAccessError.js'
 import { getTomlConfig } from '../../../utils/TomlUtils.js'
 import { sessionFactory } from '../../../factory/SessionFactory.js'
+import Dev, { COMPONENT_ARG_NAME } from './dev.js'
 
 export default class Generate {
   static args = {
-    component: Args.string({
-      description: 'Name of the component to generate',
+    [COMPONENT_ARG_NAME]: Args.string({
+      description: 'Component name(s)',
       required: true
     })
   }
@@ -36,68 +36,55 @@ export default class Generate {
     const tomlConfig = await getTomlConfig()
 
     sessionFactory(this.id, tomlConfig)
-    Generate.setSessionArgs(args, tomlConfig)
+    Dev.setSessionArgs(args, tomlConfig)
 
-    const workspaceFolder = Components.SNIPPETS_FOLDER_NAME
-    const componentName = Session.components
-    const componentFolder = join(workspaceFolder, componentName)
-    const componentRootFolder = join(getPackageRootFolder(), componentFolder)
+    for (const componentName of Session.components) {
+      const componentFolder = join(COMPONENTS_FOLDER, componentName)
+      const componentRootFolder = join(getPackageRootFolder(), componentFolder)
 
-    logger.info(`Generating "${componentName}" ${Session.targetType}`)
+      logger.info(`Generating "${componentName}" ${Session.targetType}`)
 
-    // Exit if the folder already exists
-    let folderExists = false
-    try {
-      // This will throw an error if the folder doesn't exist.
-      await access(componentRootFolder, constants.X_OK)
-      // This only runs if the previous "access" call was successful, proving the folder already exists
-      folderExists = true
-    } catch {
-      // An error is expected since the folder shouldn't exist
-    }
-
-    // Don't overwrite an existing component, throw an error
-    if (folderExists) {
-      throw new FileAccessError(`The "${componentName}" ${Session.targetType} folder already exists. Please remove it or choose a different name.`)
-    }
-
-    const cliRootFolder = getCLIRootFolderName()
-    const componentSources = join(cliRootFolder, 'resources/component-files')
-
-    const packageScope = getPackageScope()
-    const packageScopeName = packageScope.startsWith('@') ? packageScope.substring(1) : packageScope
-    const packageName = getPackageName()
-    const packageManifest = await getPackageManifest()
-    const copyFolderOptions = {
-      recursive: true,
-      jsTemplateVariables: {
-        author: packageManifest.author ? packageManifest.author : 'Archetype Themes Limited Partnership',
-        collectionName: packageName,
-        collectionScope: packageScope,
-        componentName,
-        componentType: Session.targetType,
-        componentFolder: `${workspaceFolder}/${componentName}`,
-        gitUrl: `https://github.com/${packageScopeName}/${packageName}.git`,
-        license: packageManifest.license ? packageManifest.license : 'UNLICENSED',
-        packageName: `${packageScope}/${componentName}-${Session.targetType}`
+      // Exit if the folder already exists
+      let folderExists = false
+      try {
+        // This will throw an error if the folder doesn't exist.
+        await access(componentRootFolder, constants.X_OK)
+        // This only runs if the previous "access" call was successful, proving the folder already exists
+        folderExists = true
+      } catch {
+        // An error is expected since the folder shouldn't exist
       }
+
+      // Don't overwrite an existing component, throw an error
+      if (folderExists) {
+        throw new FileAccessError(`The "${componentName}" ${Session.targetType} folder already exists. Please remove it or choose a different name.`)
+      }
+
+      const cliRootFolder = getCLIRootFolderName()
+      const componentSources = join(cliRootFolder, 'resources/component-files')
+
+      const packageScope = getPackageScope()
+      const packageScopeName = packageScope.startsWith('@') ? packageScope.substring(1) : packageScope
+      const packageName = getPackageName()
+      const packageManifest = await getPackageManifest()
+      const copyFolderOptions = {
+        recursive: true,
+        jsTemplateVariables: {
+          author: packageManifest.author ? packageManifest.author : 'Archetype Themes Limited Partnership',
+          collectionName: packageName,
+          collectionScope: packageScope,
+          componentName: componentName,
+          componentType: Session.targetType,
+          componentFolder: componentFolder,
+          gitUrl: `https://github.com/${packageScopeName}/${packageName}.git`,
+          license: packageManifest.license ? packageManifest.license : 'UNLICENSED',
+          packageName: `${packageScope}/${componentName}-${Session.targetType}`
+        }
+      }
+
+      // Copy files recursively
+      await mkdir(componentRootFolder)
+      await FileUtils.copyFolder(componentSources, componentRootFolder, copyFolderOptions)
     }
-
-    // Copy files recursively
-    await mkdir(componentRootFolder)
-    await FileUtils.copyFolder(componentSources, componentRootFolder, copyFolderOptions)
-
-    // Run npm install; this must be done, or npm will send error messages relating to monorepo integrity
-    return exec('npm install', { cwd: componentRootFolder })
-  }
-
-  static setSessionArgs (args, tomlConfig) {
-    Session.callerType = Components.COLLECTION_TYPE_NAME
-    Session.targetType = Components.COMPONENT_TYPE_NAME
-
-    if (args.component)
-      Session.components = args.component
-    else if (tomlConfig.component)
-      Session.components = tomlConfig.component
   }
 }
