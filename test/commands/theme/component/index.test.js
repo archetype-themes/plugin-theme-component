811f2a43e92c687f3a8a8d82acc955903999c70f
Frédéric Léveillée <fleveillee@gmail.com>
2024-02-28 15:31:04 -0500

    Adding oclif mocha tests for component index.js command file

---
+++ test/commands/theme/component/index.test.js
@@ -0,0 +1,18 @@
+import { expect, test } from '@oclif/test'
+import { describe } from 'mocha'
+
+describe('component', () => {
+  test
+    .stdout()
+    .command(['theme:component'])
+    .it('runs component cmd', ctx => {
+      expect(ctx.stdout).to.contain('Welcome To The Theme Component Plugin, by Archetype Themes.')
+    })
+  test
+    .stdout()
+    .command(['theme:component', '-v'])
+    .it('runs component cmd', ctx => {
+      expect(ctx.stdout).to.contain('Shopify CLI Version:')
+      expect(ctx.stdout).to.contain('Theme Component Plugin Version:')
+    })
+})
