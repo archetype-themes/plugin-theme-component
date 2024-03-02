edfbf5a2ae44fa1d04ea88a612c69fc950a4c212
Frédéric Léveillée <fleveillee@gmail.com>
2024-03-01 20:03:53 -0500

    remove debug logs

--- test/commands/theme/component/install.test.js
+++ test/commands/theme/component/install.test.js
@@ -1,15 +1,13 @@
-import { chdir, cwd } from 'node:process'
+import { chdir } from 'node:process'
 import { expect, test } from '@oclif/test'
 import { after, before, describe } from 'mocha'
 import { chDirToDefault, setupThemeRepo } from '../../../utils.js'
 
 describe('install command', function () {
   before(async function () {
-    console.log('CWD', cwd())
     this.timeout(10000)
     const themeInstallPath = await setupThemeRepo()
     chdir(themeInstallPath)
-    console.log('CWD2', cwd())
   })
 
   test
