03a8ebf67b4e209307c9562e94194e7f4b8600ba
Frédéric Léveillée <fleveillee@gmail.com>
2024-05-27 22:33:27 -0400

    We are not deploying the build to disk anymore

--- src/factory/collectionBuildFactory.js
+++ src/factory/collectionBuildFactory.js
@@ -1,7 +1,4 @@
-import { join } from 'node:path'
-import { BUILD_FOLDER_NAME } from '../config/CLI.js'
 import CollectionBuild from '../models/CollectionBuild.js'
-import { IMPORT_MAP_SNIPPET_FILENAME } from '../config/Components.js'
 import { hyphenate } from '@shopify/cli-kit/common/string'
 
 /**
@@ -11,11 +8,7 @@ import { hyphenate } from '@shopify/cli-kit/common/string'
  */
 export function collectionBuildFactory(collection) {
   const build = new CollectionBuild()
-  // Root Folder
-  build.rootFolder = join(collection.rootFolder, BUILD_FOLDER_NAME)
-  build.snippetsFolder = join(build.rootFolder, SNIPPETS_FOLDER_NAME)
   // Files
-  build.importMapFile = join(build.snippetsFolder, IMPORT_MAP_SNIPPET_FILENAME)
   build.stylesheet = `${hyphenate(collection.name)}.css`
 
   return build
