7ccf1f57ba849bb38226c0ebb837944d882bd9b6
Thomas Kelly <thomas@archetypethemes.co>
2025-01-22 09:25:41 -0500

    Fix linting errors

--- src/utilities/objects.ts
+++ src/utilities/objects.ts
@@ -4,8 +4,8 @@ export type DeepObject = {
 
 export function deepMerge(target: DeepObject, source: DeepObject): DeepObject {
   for (const key in source) {
-    if (!source.hasOwnProperty(key) || key === "__proto__" || key === "constructor") continue;
-    if (source[key] instanceof Object && target.hasOwnProperty(key) && target[key] instanceof Object) {
+    if (!Object.hasOwn(source, key) || key === "__proto__" || key === "constructor") continue;
+    if (source[key] instanceof Object && Object.hasOwn(target, key) && target[key] instanceof Object) {
       deepMerge(target[key] as DeepObject, source[key] as DeepObject)
     } else {
       target[key] = source[key]
