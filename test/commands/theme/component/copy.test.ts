df7568c94f0fe33362c80ac27088fb982fd482cb
Chris Berthe <chrisberthe@gmail.com>
2025-02-27 20:48:58 -0500

    chore: update copy.test.ts with copyright test

--- test/commands/theme/component/copy.test.ts
+++ test/commands/theme/component/copy.test.ts
@@ -1,8 +1,8 @@
-import {runCommand} from '@oclif/test'
-import {expect} from 'chai'
+import { runCommand } from '@oclif/test'
+import { expect } from 'chai'
 import * as fs from 'node:fs'
 import * as path from 'node:path'
-import {fileURLToPath} from 'node:url'
+import { fileURLToPath } from 'node:url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const fixturesPath = path.join(__dirname, '../../../fixtures')
@@ -13,34 +13,34 @@ const testThemePath = path.join(fixturesPath, 'test-theme')
 
 describe('theme component copy', () => {
   beforeEach(() => {
-    fs.cpSync(collectionPath, testCollectionPath, {recursive: true})
-    fs.cpSync(themePath, testThemePath, {recursive: true})
+    fs.cpSync(collectionPath, testCollectionPath, { recursive: true })
+    fs.cpSync(themePath, testThemePath, { recursive: true })
     process.chdir(testCollectionPath)
   })
 
   afterEach(() => {
-    fs.rmSync(testCollectionPath, {force: true, recursive: true})
-    fs.rmSync(testThemePath, {force: true, recursive: true})
+    fs.rmSync(testCollectionPath, { force: true, recursive: true })
+    fs.rmSync(testThemePath, { force: true, recursive: true })
   })
 
   it('throws an error if the cwd is not a component collection', async () => {
     process.chdir(testThemePath)
-    const {error} = await runCommand(['theme', 'component', 'copy', testThemePath])
+    const { error } = await runCommand(['theme', 'component', 'copy', testThemePath])
     expect(error).to.be.instanceOf(Error)
     expect(error?.message).to.include('Warning: Current directory does not appear to be a component collection.')
   })
 
   it('throws an error if the component.manifest.json file is not found in the theme directory', async () => {
     process.chdir(testCollectionPath)
-    fs.rmSync(path.join(testThemePath, 'component.manifest.json'), {force: true})
-    const {error} = await runCommand(['theme', 'component', 'copy', testThemePath])
+    fs.rmSync(path.join(testThemePath, 'component.manifest.json'), { force: true })
+    const { error } = await runCommand(['theme', 'component', 'copy', testThemePath])
     expect(error).to.be.instanceOf(Error)
     expect(error?.message).to.include('Error: component.manifest.json file not found in the theme directory.')
   })
 
   it('throws an error if the version of the component collection does not match the version in the component.manifest.json file', async () => {
     process.chdir(testCollectionPath)
-    const {error} = await runCommand(['theme', 'component', 'copy', testThemePath])
+    const { error } = await runCommand(['theme', 'component', 'copy', testThemePath])
     expect(error).to.be.instanceOf(Error)
     expect(error?.message).to.include('Version mismatch:')
   })
@@ -58,5 +58,37 @@ describe('theme component copy', () => {
     expect(fs.existsSync(path.join(testThemePath, 'assets', 'to-be-copied.js'))).to.be.true
 
     expect(fs.existsSync(path.join(testThemePath, 'snippets', 'not-to-be-copied.liquid'))).to.be.false
+  })
+
+  it('adds copyright information to copied files', async () => {
+    const packageJsonPath = path.join(testCollectionPath, 'package.json')
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
+
+    packageJson.copyright = {
+      author: 'Test Author',
+      license: 'MIT'
+    }
+
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
+
+    const manifestPath = path.join(testThemePath, 'component.manifest.json')
+    const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'))
+    manifest.collections["@archetype-themes/test-collection"].version = "1.0.1"
+    fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2))
+
+    await runCommand(['theme', 'component', 'copy', testThemePath])
+
+    const cssContent = fs.readFileSync(path.join(testThemePath, 'assets', 'to-be-copied.css'), 'utf8')
+    const year = new Date().getFullYear()
+    const cssExpectedHeader = `/* @archetype-themes/test-collection v1.0.1 | Copyright © ${year} Test Author | MIT */`
+    expect(cssContent.startsWith(cssExpectedHeader)).to.be.true
+
+    const jsContent = fs.readFileSync(path.join(testThemePath, 'assets', 'to-be-copied.js'), 'utf8')
+    const jsExpectedHeader = `// @archetype-themes/test-collection v1.0.1 | Copyright © ${year} Test Author | MIT`
+    expect(jsContent.startsWith(jsExpectedHeader)).to.be.true
+
+    const liquidContent = fs.readFileSync(path.join(testThemePath, 'snippets', 'to-be-copied.liquid'), 'utf8')
+    const liquidExpectedHeader = `{% # @archetype-themes/test-collection v1.0.1 | Copyright © ${year} Test Author | MIT %}`
+    expect(liquidContent.startsWith(liquidExpectedHeader)).to.be.true
   })
 })
